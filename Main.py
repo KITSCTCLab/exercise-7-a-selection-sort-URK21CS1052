703001500224090620c3b2965156d9e0e3e00e55
URK21CS1052 <110341790+URK21CS1052@users.noreply.github.com>
2022-09-12 15:14:17 +0530

    intial commit

--- Main.py
+++ Main.py
@@ -1,9 +1,15 @@
 from typing import List
-
 def selectionSort(array, size) -> List[int]:
-  # Write your code here
+  for step in range(size):
+          min_idx = step
+          for i in range(step + 1, size):
+              # to sort in descending order, change > to < in this line
+              # select the minimum element in each loop
+              if array[i] < array[min_idx]:
+                  min_idx = i
+          # put min at the correct position
+          (array[step], array[min_idx]) = (array[min_idx], array[step])
 
-# Do not change the following code
 input_data = input()
 data = []
 for item in input_data.split(', '):
